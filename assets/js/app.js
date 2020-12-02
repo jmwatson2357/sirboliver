a8283bc22415be836f703e0df27167004bef0874
Michael Watson <54993615+jmwatson2357@users.noreply.github.com>
2020-12-02 14:03:26 -0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+// alert("Connected");
+
+let selection = document.querySelector("#myImage");
+let imgChange = document.querySelector("#image-size");
+
+selection.addEventListener("change", e => {
+    e.preventDefault()
+    const selectChoice = selection.value;
+    console.log(selectChoice);
+    if(selectChoice === "opt1"){
+        imgChange.src = "assets/images/myHeadshot.jpg";
+    } else if (selectChoice === "opt2") {
+        imgChange.src = "assets/images/myBeard.jpg";
+    } else if (selectChoice === "opt3") {
+        imgChange.src = "assets/images/myMemoji.jpg";
+    } else if (selectChoice === "opt4") {
+        imgChange.src = "assets/images/myCynide.jpg";
+    } else {
+        imgChange.src = "assets/images/myHeadshot.jpg"
+    }
+})
